1f6b0d4de2cc2e674743ae149b3bc4249b74521c
Mine <bsllmine@gmail.com>
2025-05-01 19:17:10 +0300

    Update reorder_predict.py

--- nortwind_api_project/train_model/reorder_predict.py
+++ nortwind_api_project/train_model/reorder_predict.py
@@ -13,7 +13,7 @@ import os
 import joblib
 
 # Connect to the PostgreSQL database
-db_url = "postgresql://postgres:12345@localhost:5432/Gyk1"
+db_url = "postgresql://postgres:***@localhost:***/Gyk1"
 engine = create_engine(db_url)
 
 # SQL query: customer-level purchase summary
