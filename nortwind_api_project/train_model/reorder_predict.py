2c0315eb01939281f6f53ed1dd51c607fa3a08b5
Mine <bsllmine@gmail.com>
2025-05-01 19:23:20 +0300

    db_url changed

--- nortwind_api_project/train_model/reorder_predict.py
+++ nortwind_api_project/train_model/reorder_predict.py
@@ -13,7 +13,15 @@ import os
 import joblib
 
 # Connect to the PostgreSQL database
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 db_url = "postgresql://postgres:***@localhost:***/Gyk1"
+=======
+db_url = "postgresql://postgres:***@localhost:5432/***"
+>>>>>>> Stashed changes
+=======
+db_url = "postgresql://postgres:***@localhost:5432/***"
+>>>>>>> Stashed changes
 engine = create_engine(db_url)
 
 # SQL query: customer-level purchase summary
